733e42bdd7ba2739a60febd58e489b61083c7b71
Alpcan Aydin <alpcan@alpcanaydin.com>
2020-02-19 09:40:14 +0300

    Storybook and Jest config added.

---
+++ .storybook/main.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {},
+};
+
+global.__PATH_PREFIX__ = '';
+
+module.exports = {
+  stories: ['../src/components/**/*.stories.tsx'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    return config;
+  },
+};
---
+++ src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Footer from './Footer';
+
+export default {
+  component: Footer,
+  title: 'Footer',
+};
+
+export const withDefault = () => <Footer />;
