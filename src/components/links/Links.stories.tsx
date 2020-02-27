6f58f4d9f0614de07765d1e3f9e232634b125c8e
Hasan Aydoğdu <hsnaydd@gmail.com>
2020-02-27 11:01:07 +0300

    Add LinkBrand component

--- src/components/links/Links.stories.tsx
+++ src/components/links/Links.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import LinkSecondary from './LinkSecondary';
 import LinkLight from './LinkLight';
+import LinkBrand from './LinkBrand';
 
 const Wrapper = (storyFn: any) => (
   <div style={{ padding: 8, backgroundColor: '#101013' }}>{storyFn()}</div>
@@ -14,3 +15,4 @@ export default {
 
 export const secondary = () => <LinkSecondary to="./">Hello</LinkSecondary>;
 export const white = () => <LinkLight to="./">Hello</LinkLight>;
+export const brand = () => <LinkBrand to="./">Hello</LinkBrand>;
