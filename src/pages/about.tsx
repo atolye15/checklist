b45fb70a1eb0138bc8477b0955364a371261a6ae
Alpcan Aydin <alpcan@alpcanaydin.com>
2020-03-03 15:54:24 +0300

    Styles updated.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,7 +8,7 @@ import ChecklistCard from '../components/ChecklistCard';
 
 const About: FC = () => (
   <Layout>
-    <div className="row u-justify-content-center u-padding-top-xlarge u-padding-bottom-2xlarge u-text-style-subtitle">
+    <div className="row u-justify-content-center u-padding-top-xlarge u-padding-bottom-2xlarge">
       <div className="col col--lg-8">
         <div className="u-text-align-center u-padding-bottom-medium">
           <img src={aboutImage} alt="What is Checklist?" width="710" height="430" />
@@ -18,26 +18,29 @@ const About: FC = () => (
           What is <br /> Atolye15 Checklist
         </h1>
 
-        <p>
-          We love to automate anything if it can be automated. For the things we can’t automate
-          (yet) we have a protocol. These protocols standardise our workflow so that everyone in
-          Atölye15 works on a standard.
-        </p>
+        <div className="u-text-style-subtitle">
+          <p>
+            We love to automate anything if it can be automated. For the things we can’t automate
+            (yet) we have a protocol. These protocols standardise our workflow so that everyone in
+            Atölye15 works on a standard.
+          </p>
 
-        <p>
-          We have been designing, developing & managing projects that vary in size for over 10 years
-          now. Working on a different scale projects brought us some priceless experiences that led
-          us to a greater success on the following projects. We updated our workflow, the way we
-          work, even the hours we work. We try to adapt to the ongoing growth on technology and we
-          are always on track with keeping our protocols updated.
-        </p>
+          <p>
+            We have been designing, developing & managing projects that vary in size for over 10
+            years now. Working on a different scale projects brought us some priceless experiences
+            that led us to a greater success on the following projects. We updated our workflow, the
+            way we work, even the hours we work. We try to adapt to the ongoing growth on technology
+            and we are always on track with keeping our protocols updated.
+          </p>
 
-        <p>
-          This was an internal tool that we still use on every department but we’ve decided to share
-          this with the community so that we can help each other out, please feel free to suggest a
-          checklist or an improvement on a checklist (on its own page). We will consider every
-          suggestion and implement the ones that we think fits our needs and the way we work.
-        </p>
+          <p>
+            This was an internal tool that we still use on every department but we’ve decided to
+            share this with the community so that we can help each other out, please feel free to
+            suggest a checklist or an improvement on a checklist (on its own page). We will consider
+            every suggestion and implement the ones that we think fits our needs and the way we
+            work.
+          </p>
+        </div>
 
         <a
           href="https://www.atolye15.com"
@@ -52,7 +55,7 @@ const About: FC = () => (
       <h2 className="u-margin-bottom-medium">Check out these checklists before you go!</h2>
       <div className="row u-padding-bottom-large">
         <div className="col col--lg-6">
-          <Blockquote className="u-text-style-large-body">
+          <Blockquote>
             We have developed a protocol for anyone to suggest a checklist. We told you we have a
             protocol for almost everything!{' '}
             <span role="img" aria-labelledby="rocket">
