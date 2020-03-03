48a8dac733d18f391a0c814d980f2fee46be9f45
Hasan Aydoğdu <hsnaydd@gmail.com>
2020-03-03 11:36:55 +0300

    Refactor Pagination component

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,20 +1,40 @@
 import React, { FC } from 'react';
-import { Link } from 'gatsby';
+import cx from 'classnames';
+
+import IconLinkButton from '../Button/IconButton/IconLinkButton';
+
+import './c-pagination.scss';
 
 interface Props {
   totalPages: number;
   currentPage: number;
+  className?: string;
 }
 
-const Pagination: FC<Props> = ({ totalPages, currentPage }) => (
-  <div>
-    {currentPage === 2 && <Link to="/">Previous checklists</Link>}
-    {currentPage > 2 && <Link to={`/checklists/${currentPage - 1}`}>Previous checklists</Link>}
-    <br />
-    {totalPages > 1 && currentPage !== totalPages && (
-      <Link to={`/checklists/${currentPage + 1}`}>Next checklists</Link>
+const Pagination: FC<Props> = ({ className, totalPages, currentPage }) => (
+  <nav className={cx('c-pagination', className)} aria-label="Checklists Navigation">
+    {currentPage > 1 && (
+      <IconLinkButton
+        to={currentPage === 2 ? '/' : `/checklists/${currentPage - 1}`}
+        className="c-pagination__previous"
+        iconName="arrow-left"
+        theme="tertiary"
+        size="medium"
+        aria-label="Previous checklists"
+      />
     )}
-  </div>
+
+    {totalPages > 1 && currentPage !== totalPages && (
+      <IconLinkButton
+        to={`/checklists/${currentPage + 1}`}
+        className="c-pagination__next"
+        iconName="arrow-right"
+        theme="tertiary"
+        size="medium"
+        aria-label="Next checklists"
+      />
+    )}
+  </nav>
 );
 
 export default Pagination;
