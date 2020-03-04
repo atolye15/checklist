1618034e1b5966accb3cd73bac4b10a0d46e3033
Hasan Aydoğdu <hsnaydd@gmail.com>
2020-03-04 14:27:01 +0300

    Add `AsAnchor` variants of the links

--- src/components/links/Links.stories.tsx
+++ src/components/links/Links.stories.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import LinkSecondary from './LinkSecondary';
 import LinkLight from './LinkLight';
 import LinkBrand from './LinkBrand';
+import LinkSecondaryAsAnchor from './LinkSecondary/LinkSecondaryAsAnchor';
+import LinkLightAsAnchor from './LinkLight/LinkLightAsAnchor/LinkLightAsAnchor';
+import LinkBrandAsAnchor from './LinkBrand/LinkBrandAsAnchor';
 
 const Wrapper = (storyFn: any) => (
   <div style={{ padding: 8, backgroundColor: '#101013' }}>{storyFn()}</div>
@@ -13,6 +16,21 @@ export default {
   decorators: [Wrapper],
 };
 
-export const secondary = () => <LinkSecondary to="./">Hello</LinkSecondary>;
-export const white = () => <LinkLight to="./">Hello</LinkLight>;
-export const brand = () => <LinkBrand to="./">Hello</LinkBrand>;
+export const secondary = () => (
+  <>
+    <LinkSecondary to="./">Hello</LinkSecondary>
+    <LinkSecondaryAsAnchor href="./">As Anchor</LinkSecondaryAsAnchor>
+  </>
+);
+export const white = () => (
+  <>
+    <LinkLight to="./">Hello</LinkLight>
+    <LinkLightAsAnchor href="./">As Anchor</LinkLightAsAnchor>
+  </>
+);
+export const brand = () => (
+  <>
+    <LinkBrand to="./">Hello</LinkBrand>
+    <LinkBrandAsAnchor href="./">As Anchor</LinkBrandAsAnchor>
+  </>
+);
