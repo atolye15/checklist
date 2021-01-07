062a34c680e2b2af5b7d4d4be70a7ebabb8a4f12
Uğur Eren <mymsraid@gmail.com>
2021-01-07 23:51:39 +0300

    Added weight prop for text component
    Added weight prop for text component
    - thin
    - regular
    - medium
    - semi bold
    - bold
    Weights added

--- src/Components/Text/Text.tsx
+++ src/Components/Text/Text.tsx
@@ -5,10 +5,11 @@ import {withTheme, ThemeType} from '../../Theming';
 interface Props extends TextProps {
   theme: ThemeType;
   light?: boolean;
+  weight?: 'thin' | 'regular' | 'medium' | 'semiBold' | 'bold';
 }
 
 const Text: React.FC<Props> = (props) => {
-  const {theme, light, style, children, ...textProps} = props;
+  const {theme, light, weight, style, children, ...textProps} = props;
 
   return (
     <NativeText
@@ -17,6 +18,11 @@ const Text: React.FC<Props> = (props) => {
           color: light ? theme.colors.textLight : theme.colors.text,
           ...theme.fonts.regular,
         },
+        weight === 'thin' && theme.fonts.thin,
+        weight === 'regular' && theme.fonts.regular,
+        weight === 'medium' && theme.fonts.medium,
+        weight === 'semiBold' && theme.fonts.semiBold,
+        weight === 'bold' && theme.fonts.bold,
         style,
       ]}
       {...textProps}
