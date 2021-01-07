b2482c5b57f6a903c94f5b0206af8d48ac52ea50
Uğur Eren <mymsraid@gmail.com>
2021-01-08 00:42:09 +0300

    Create Chip Component
    - Created the Chip Component file
    - Added the Chip Component to the index of Components

---
+++ src/Components/Chip/Chip.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const Chip: React.FC = () => {
+  return <View />;
+};
+
+export default Chip;
--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,4 +1,5 @@
 import Text from './Text/Text';
 import Card from './Card/Card';
+import Chip from './Chip/Chip';
 
-export {Text, Card};
+export {Text, Card, Chip};
