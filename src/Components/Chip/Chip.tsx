72dde14711ad5460f2fcad78f75b025df237b539
Uğur Eren <mymsraid@gmail.com>
2021-01-08 00:47:07 +0300

    Basic Chip Component
    Chip component created based on the card component's tags

--- src/Components/Chip/Chip.tsx
+++ src/Components/Chip/Chip.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import {View} from 'react-native';
+import memoizeOne from 'memoize-one';
+import Text from '../Text/Text';
+import styles from './styles';
 
-const Chip: React.FC = () => {
-  return <View />;
+interface Props {
+  color: string;
+  title: string;
+}
+
+const Chip: React.FC<Props> = (props) => {
+  const {color, title} = props;
+
+  const chipTextStlyes = getChipTextStyles(color);
+
+  return (
+    <View style={styles.chip}>
+      <Text light weight="bold" style={chipTextStlyes}>
+        #{title}
+      </Text>
+    </View>
+  );
 };
 
-export default Chip;
+const getChipTextStyles = memoizeOne((color) => [styles.chipText, {backgroundColor: color}]);
+
+export default React.memo(Chip);
