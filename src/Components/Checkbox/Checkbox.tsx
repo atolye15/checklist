b7572b1358f159a75416ee87d63ff3b6ee313b9b
Uğur Eren <mymsraid@gmail.com>
2021-01-08 10:17:54 +0300

    Create Checkbox Component
    - Created the Checkbox Component
    - Added required images to the assets (tick)

---
+++ src/Components/Checkbox/Checkbox.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const Checkbox: React.FC = () => {
+  return <View />;
+};
+
+export default Checkbox;
--- src/Components/index.ts
+++ src/Components/index.ts
@@ -2,5 +2,6 @@ import Text from './Text/Text';
 import Card from './Card/Card';
 import Chip from './Chip/Chip';
 import Button from './Button/Button';
+import Checkbox from './Checkbox/Checkbox';
 
-export {Text, Card, Chip, Button};
+export {Text, Card, Chip, Button, Checkbox};
