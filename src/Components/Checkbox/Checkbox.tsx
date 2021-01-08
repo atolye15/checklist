39bd806a26f5b1a3c87b66f9d7bba89108eb81a3
Uğur Eren <mymsraid@gmail.com>
2021-01-08 10:54:36 +0300

    Implement memoization for performance bump
    - Code cleaning
    - memoize-one package used in the Checkbox Component for performance improvement

--- src/Components/Checkbox/Checkbox.tsx
+++ src/Components/Checkbox/Checkbox.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {TouchableOpacity, TouchableOpacityProps, Image} from 'react-native';
+import memoizeOne from 'memoize-one';
 import {withTheme, ThemeType} from '../../Theming';
 import styles from './styles';
 
@@ -11,14 +12,22 @@ interface Props extends TouchableOpacityProps {
 const Checkbox: React.FC<Props> = (props) => {
   const {theme, checked, ...restProps} = props;
 
+  const memoizedStyles = getMemoizedStyles(checked, theme);
+
   return (
-    <TouchableOpacity style={[styles.container, checked && styles.containerChecked]} {...restProps}>
-      <Image
-        source={require('../../Assets/Images/tick.png')}
-        style={[styles.tick, checked && styles.tickChecked]}
-      />
+    <TouchableOpacity style={memoizedStyles.container} {...restProps}>
+      <Image source={require('../../Assets/Images/tick.png')} style={memoizedStyles.tick} />
     </TouchableOpacity>
   );
 };
+
+const getMemoizedStyles = memoizeOne((checked, theme) => ({
+  container: [
+    styles.container,
+    {borderColor: theme.colors.textLight},
+    checked && {borderColor: theme.colors.primary, backgroundColor: theme.colors.primary},
+  ],
+  tick: [styles.tick, checked && {tintColor: theme.colors.textLight}],
+}));
 
 export default React.memo(withTheme(Checkbox));
--- src/Components/Checkbox/styles.ts
+++ src/Components/Checkbox/styles.ts
@@ -1,25 +1,19 @@
 import {StyleSheet} from 'react-native';
 
+const transparent = 'transparent';
+
 const styles = StyleSheet.create({
   container: {
     borderWidth: 1,
     borderRadius: 24,
-    borderColor: 'white',
-    backgroundColor: 'transparent',
+    backgroundColor: transparent,
     marginTop: 2,
     overflow: 'hidden',
-  },
-  containerChecked: {
-    borderColor: 'red',
-    backgroundColor: 'red',
   },
   tick: {
     width: 22,
     height: 22,
-    tintColor: 'transparent',
-  },
-  tickChecked: {
-    tintColor: 'white',
+    tintColor: transparent,
   },
 });
 
